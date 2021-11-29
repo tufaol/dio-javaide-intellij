4b63b18cbc25e8c7b0c2ed5aad36c2510d6850e2
tufaol <tufaol@gmail.com>
2021-11-29 10:08:43 -0300

    Refatorado classe Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -1,6 +1,7 @@
 package br.com.dio;
 
 import br.com.dio.model.Gato;
+import br.com.dio.model.Livro;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
@@ -11,44 +12,12 @@ public class PrimeiroPrograma {
         Livro livro = new Livro("Iniciando em Java", 575);
         System.out.println(livro);
 
+        Livro livro1 = new Livro("Iniciando em JavaScript", 482);
+        System.out.println(livro1);
+
 
         /*int a = 13;
         double b = 2.6;
         System.out.println("Olá Mundo Intellij " + (b + a));*/
     }
 }
-
-class Livro{
-    private String nome;
-    private Integer numPaginas;
-
-    public Livro(String nome, Integer numPaginas) {
-        this.nome = nome;
-        this.numPaginas = numPaginas;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public Integer getNumPaginas() {
-        return numPaginas;
-    }
-
-    public void setNumPaginas(Integer numPaginas) {
-        this.numPaginas = numPaginas;
-    }
-
-    @Override
-    public String toString() {
-        return "Livro{" +
-                "nome='" + nome + '\'' +
-                ", numPaginas=" + numPaginas +
-                '}';
-    }
-}
-
