3a48868ba4b30f657a4185131303628cf406591b
tufaol <tufaol@gmail.com>
2021-11-29 09:56:51 -0300

    Iniciando projeto

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+        /*int a = 13;
+        double b = 2.6;
+        System.out.println("Olá Mundo Intellij " + (b + a));*/
+    }
+}
---
+++ src/br/com/dio/model/Gato.java
@@ -0,0 +1,40 @@
+package br.com.dio.model;
+
+public class Gato {
+    String nome;
+    String cor;
+    Integer idade;
+
+    public Gato() {
+    }
+
+    public Gato(String nome, String cor, Integer idade) {
+        this.nome = nome;
+        this.cor = cor;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+}
